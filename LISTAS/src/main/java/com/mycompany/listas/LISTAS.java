1119866ca586edee345a18ad07493345758fec58
daniel1998-gt <danielmancilla025@gmail.com>
2024-02-17 10:43:02 -0600

    daniel1998-gt
    AGREGRAR LISTAS SIMPLES DE PROGRAMACION
    
    Co-Authored-By: tex-44 <125715501+tex-44@users.noreply.github.com>

---
+++ LISTAS/src/main/java/com/mycompany/listas/LISTAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listas;
+
+/**
+ *
+ * @author DanielMancilla 98
+ */
+public class LISTAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
